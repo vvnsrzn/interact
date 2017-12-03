f4ab5e8a4cf7981df8bc75a4de2bf857a2863906
Felix Macherez <felixmacherez@MacBook-Pro-de-Felix.local>
2017-12-03 18:55:54 +0100

    hermanitos

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// target elements with the "draggable" class
-interact('.draggable')
-.draggable({
-  // enable inertial throwing
-  inertia: true,
-  // keep the element within the area of it's parent
-  restrict: {
-    restriction: "parent",
-    endOnly: true,
-    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-  },
-  // enable autoScroll
-  autoScroll: true,
-
-  // call this function on every dragmove event
-  onmove: dragMoveListener,
-  // call this function on every dragend event
-  onend: function (event) {
-    var textEl = event.target.querySelector('p');
-
-    textEl && (textEl.textContent =
-      'moved a distance of '
-      + (Math.sqrt(event.dx * event.dx +
-                   event.dy * event.dy)|0) + 'px');
-  }
-});
-
 function dragMoveListener (event) {
   var target = event.target,
       // keep the dragged position in the data-x/data-y attributes
@@ -41,5 +14,30 @@ function dragMoveListener (event) {
   target.setAttribute('data-y', y);
 }
 
-// this is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener;
+interact('.resize-drag')
+.draggable({
+  onmove: window.dragMoveListener
+})
+.resizable({
+  edges: { left: true, right: true, bottom: true, top: true }
+})
+.on('resizemove', function (event) {
+  var target = event.target;
+      x = (parseFloat(target.getAttribute('data-x')) || 0),
+      y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+  // update the element's style
+  target.style.width  = event.rect.width + 'px';
+  target.style.height = event.rect.height + 'px';
+
+  // translate when resizing from top or left edges
+  x += event.deltaRect.left;
+  y += event.deltaRect.top;
+
+  target.style.webkitTransform = target.style.transform =
+      'translate(' + x + 'px,' + y + 'px)';
+
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+  target.textContent = event.rect.width + '×' + event.rect.height;
+});
