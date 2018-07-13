44b46a1834b6df4232335d4fff80211687c85dd4
VivianSolide <vivian.sarazin@gmail.com>
2018-07-13 17:07:45 +0200

    script :: toggling

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-function dragMoveListener (event) {
-  var target = event.target,
-      // keep the dragged position in the data-x/data-y attributes
-      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+function dragMoveListener(event) {
+	var target = event.target,
+		// keep the dragged position in the data-x/data-y attributes
+		x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
+		y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
 
-  // translate the element
-  target.style.webkitTransform =
-  target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
+	// translate the element
+	target.style.webkitTransform = target.style.transform =
+		"translate(" + x + "px, " + y + "px)";
 
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
+	// update the posiion attributes
+	target.setAttribute("data-x", x);
+	target.setAttribute("data-y", y);
 }
 
-interact('.size')
-.draggable({
-  onmove: window.dragMoveListener
-})
-.resizable({
-  edges: { left: true, right: true, bottom: true, top: true },
-  preserveAspectRatio: true
-})
-.on('resizemove', function (event) {
-  var target = event.target;
-      x = (parseFloat(target.getAttribute('data-x')) || 0),
-      y = (parseFloat(target.getAttribute('data-y')) || 0);
+interact(".size")
+	.draggable({
+		onmove: window.dragMoveListener
+	})
+	.resizable({
+		edges: { left: true, right: true, bottom: true, top: true },
+		preserveAspectRatio: true
+	})
+	.on("resizemove", function(event) {
+		var target = event.target;
+		(x = parseFloat(target.getAttribute("data-x")) || 0),
+			(y = parseFloat(target.getAttribute("data-y")) || 0);
 
-  // update the element's style
-  target.style.width  = event.rect.width + 'px';
-  target.style.height = event.rect.height + 'px';
+		// update the element's style
+		target.style.width = event.rect.width + "px";
+		target.style.height = event.rect.height + "px";
 
-  // translate when resizing from top or left edges
-  x += event.deltaRect.left;
-  y += event.deltaRect.top;
+		// translate when resizing from top or left edges
+		x += event.deltaRect.left;
+		y += event.deltaRect.top;
 
-  target.style.webkitTransform = target.style.transform =
-      'translate(' + x + 'px,' + y + 'px)';
+		target.style.webkitTransform = target.style.transform =
+			"translate(" + x + "px," + y + "px)";
 
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-  target.textContent = event.rect.width + '×' + event.rect.height;
+		target.setAttribute("data-x", x);
+		target.setAttribute("data-y", y);
+		target.textContent = event.rect.width + "×" + event.rect.height;
+	});
+
+$(".open").click(function() {
+	$(this)
+		.removeClass("open")
+		.addClass("closed");
+});
+
+$(".closed").click(function() {
+	$(this)
+		.removeClass("closed")
+		.addClass("open");
 });
